7c0d6228e2bee480da97e6a9c313380663b4201c
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 15:38:18 +0100

    Imprementaçao para calculo de area, perimetro e diagonal de um retangulo.

--- CalculoRetangulo/Program.cs
+++ CalculoRetangulo/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace CalculoRetangulo
+﻿using System.Globalization;
+
+namespace CalculoRetangulo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double baseR, altura, area, perimetro, diagonal;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Base do retangulo: ");
+            baseR = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine(), CI);
+
+            area = baseR * altura;
+            perimetro = 2 * (baseR + altura);
+            diagonal = Math.Sqrt(Math.Pow(baseR, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine($"AREA = {area.ToString("F4", CI)}");
+            Console.WriteLine($"PERIMETRO = {perimetro.ToString("F4", CI)}");
+            Console.WriteLine($"DIAGONAL = {diagonal.ToString("F4", CI)}");
         }
     }
 }
